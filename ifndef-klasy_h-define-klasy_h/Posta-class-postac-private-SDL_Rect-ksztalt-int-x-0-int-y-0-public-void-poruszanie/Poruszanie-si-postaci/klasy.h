921f7e219878e8d6921d000d7b861b6458daa3cf
Burn44 <fr@archiplan>
2013-12-19 00:10:37 +0100

    Create klasy.h

---
+++ ifndef-klasy_h-define-klasy_h/Posta-class-postac-private-SDL_Rect-ksztalt-int-x-0-int-y-0-public-void-poruszanie/Poruszanie-si-postaci/klasy.h
@@ -0,0 +1,34 @@
+#ifndef klasy_h
+#define klasy_h
+
+// Postać
+class postac
+{
+private:
+	SDL_Rect ksztalt;
+
+	int x = 0;
+	int y = 0;
+
+public:
+	void poruszanie(); // Poruszanie się postacią
+
+};
+
+// Przycisk
+
+class przycisk
+{
+private:
+	SDL_Rect ksztalt;
+
+	int x = 0;
+	int y = 0;
+public:
+
+	void wgraj_przycisk(int , int , SDL_Surface* , SDL_Surface* , SDL_Window*);
+
+	bool wydarzenie(SDL_Event);
+};
+
+#endif
