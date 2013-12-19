0c33d55074c68d2641a8ffa6a7930b626fa41bc8
Burn44 <fr@archiplan>
2013-12-19 02:21:07 +0100

    Update funkcje.cpp

--- funkcje.cpp
+++ funkcje.cpp
@@ -4,7 +4,7 @@
 #include "klasy.h"
 
 // Wczytuje na ekran obrazek z Surface
-void wgraj_surface(int x, int y, SDL_Surface* surface, SDL_Surface* przeznaczenie)
+void wgraj_surface(int x, int y, SDL_Surface* surface, SDL_Surface* przeznaczenie, SDL_Window* okno)
 {
 	SDL_Rect polozenie;
 
@@ -12,10 +12,12 @@ void wgraj_surface(int x, int y, SDL_Surface* surface, SDL_Surface* przeznaczeni
 	polozenie.y = y;
 
 	SDL_BlitSurface(surface, NULL, przeznaczenie, &polozenie);
+	SDL_UpdateWindowSurface(okno);
 }
 //funkcja losujaca -> przedzial + poczatek 
-int losowanie(int przedzial , int liczba_poczatek)
+int losowanie(int przedzial, int liczba_poczatek)
 {
-	int b = (rand() % przedzial) + liczba_poczatek; 
+	int b = (rand() % przedzial) + liczba_poczatek;
 	return  b;
 }
+
